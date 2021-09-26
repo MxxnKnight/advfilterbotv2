69631052c24982e4a223fae6977855d5189a3019
Adarshdarsh <91146761+Adarshdarsh@users.noreply.github.com>
2021-09-27 04:11:30 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,10 +7,12 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You cant add me to your group😋
+
+Maintained by @dl_black
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@dl_black
 </i>"""    
     
     HELP_TEXT = """
@@ -43,16 +45,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@dl_black
 """
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https:@dl_black</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="tharilla">Click Me</a></i>
 """
